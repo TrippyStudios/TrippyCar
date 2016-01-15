74a8b2f6088adbaed9efa9b5748827506980b2c7
Malcolm Maude <sushihog@gmail.com>
2016-01-14 20:30:24 -0600

    Revert "Revert "added trippy car""
    This reverts commit 11a4390c19f58daeb0df2f01782429d1c7ebf6ce.

---
+++ Round Space Station 1/Assets/New Folder/play.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class play : MonoBehaviour {
+
+	// Use this for initialization
+	public void changeScene () {
+        SceneManager.LoadScene(2);
+	}
+}
