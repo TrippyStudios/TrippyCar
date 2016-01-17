952a0b39f79f1947b137a67d5359af4f4486a626
Malcolm Maude <sushihog@gmail.com>
2016-01-17 12:40:31 -0600

    SECRETS

---
+++ Round Space Station 1/Assets/New Folder/Secret/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 15;
+    private Vector3 moveDir;
+
+	void Update () {
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+	}
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
+    }
+}
